f2befb74728f4bfca2f5bfb787fe81203b2335ea
Christopher-Robin Ebbinghaus <git@cebbinghaus.com>
2021-09-29 12:49:03 +1000

    Changed test file name and Changed Jest options

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,9 @@ module.exports = {
 	preset: 'ts-jest',
 	testEnvironment: 'node',
 	rootDir: "tests",
-	testMatch: ["**/*.test.ts"]
+	testMatch: ["**/*.test.ts"],
+
+	coverageDirectory: "coverage",
+	coverageProvider: "v8",
+	collectCoverageFrom: ["src/**.ts"],
 };
