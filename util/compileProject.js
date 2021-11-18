428d1d6db63fec96a6e44a2d366ac1a71b77c67d
Christopher-Robin Ebbinghaus <git@cebbinghaus.com>
2021-11-19 01:06:07 +1100

    Fixed Compiling script to work with Typescript API

--- util/compileProject.js
+++ util/compileProject.js
@@ -1,40 +1,67 @@
-import * as ts from "typescript";
-import * as fs from "fs"
-
+import ts from "typescript";
+import * as fs from "fs";
+import glob from "glob";
 
 /**
  * Compiles Project
- * @param {string[]} fileNames 
- * @param {ts.CompilerOptions} options 
+ * @param {string[]} fileNames
+ * @param {ts.CompilerOptions} options
+ * @param {ts.CompilerOptions} options
  */
-function compile(fileNames, options){
-  let program = ts.createProgram(fileNames, options);
-  let emitResult = program.emit();
+function compile(fileNames, options) {
+	const host = ts.createCompilerHost(options);
+	let program = ts.createProgram(fileNames, options, host);
+	let emitResult = program.emit();
 
-  let allDiagnostics = ts
-    .getPreEmitDiagnostics(program)
-    .concat(emitResult.diagnostics);
+	let allDiagnostics = ts
+		.getPreEmitDiagnostics(program)
+		.concat(emitResult.diagnostics);
 
-  allDiagnostics.forEach(diagnostic => {
-    if (diagnostic.file) {
-      let { line, character } = ts.getLineAndCharacterOfPosition(diagnostic.file, diagnostic.start);
-      let message = ts.flattenDiagnosticMessageText(diagnostic.messageText, "\n");
-      console.log(`${diagnostic.file.fileName} (${line + 1},${character + 1}): ${message}`);
-    } else {
-      console.log(ts.flattenDiagnosticMessageText(diagnostic.messageText, "\n"));
-    }
-  });
+	allDiagnostics.forEach((diagnostic) => {
+		if (diagnostic.file) {
+			let { line, character } = ts.getLineAndCharacterOfPosition(
+				diagnostic.file,
+				diagnostic.start
+			);
+			let message = ts.flattenDiagnosticMessageText(
+				diagnostic.messageText,
+				"\n"
+			);
+			console.log(
+				`${diagnostic.file.fileName} (${line + 1},${
+					character + 1
+				}): ${message}`
+			);
+		} else {
+			console.log(
+				ts.flattenDiagnosticMessageText(diagnostic.messageText, "\n")
+			);
+		}
+	});
 
-  let exitCode = emitResult.emitSkipped ? 1 : 0;
-  console.log(`Process exiting with code '${exitCode}'.`);
-  process.exit(exitCode);
+	let exitCode = emitResult.emitSkipped ? 1 : 0;
+	console.log(`Process exiting with code '${exitCode}'.`);
+	process.exit(exitCode);
 }
 
-fs.readFileSync("../tsconfig.json");
+/**
+ * @type {../tsconfig.json}
+ */
+const tsconfig = JSON.parse(fs.readFileSync("./tsconfig.json"));
 
-compile(process.argv.slice(2), {
-  noEmitOnError: true,
-  noImplicitAny: true,
-  target: ts.ScriptTarget.ES5,
-  module: ts.ModuleKind.CommonJS
+const compilerOptions = tsconfig.compilerOptions;
+
+glob("src/**.ts", (err, files) => {
+	if (err) {
+		console.error(err);
+		return;
+	}
+	const options = {
+		"Classic": ts.ModuleResolutionKind.Classic,
+		"Node": ts.ModuleResolutionKind.NodeJs
+	};
+
+	compilerOptions["moduleResolution"] = options[compilerOptions["moduleResolution"]];
+
+	compile(files, compilerOptions);
 });
