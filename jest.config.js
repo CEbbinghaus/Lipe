f13176cf8a657615eb3e5c983bf496b8d004634f
CEbbinghaus <git@cebbinghaus.com>
2022-03-06 16:36:39 +1100

    Adding Workflows for Building/Testing/Publishing (#8)

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,25 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-	preset: 'ts-jest',
-	testEnvironment: 'node',
-	rootDir: "tests",
+	preset: "ts-jest",
+	testEnvironment: "node",
+	// rootDir: "tests",
 	testMatch: ["**/*.test.ts"],
+
+	collectCoverage: true,
+	collectCoverageFrom: ["src/**/*.ts"],
+
+	// // coverageProvider: ["v8"],
+	// coverageReporters: ["clover", "json", "lcov", "text"],
+
+	coverageThreshold: {
+		global: {
+			branches: 90,
+			functions: 90,
+			lines: 90,
+			statements: 90,
+		},
+	},
 
 	coverageDirectory: "coverage",
 	coverageProvider: "v8",
-	collectCoverageFrom: ["src/**.ts"],
 };
