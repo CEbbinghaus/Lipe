af0307c19a8fde6a6389b5a0ace00e0105c93f05
CEbbinghaus <git@cebbinghaus.com>
2022-10-09 17:48:07 +1100

    cleaned up jest file

--- jest.config.js
+++ jest.config.js
@@ -3,24 +3,11 @@ module.exports = {
 	resolver: "jest-ts-webcompat-resolver",
 	preset: "ts-jest",
 	testEnvironment: "node",
-	// rootDir: "tests",
-	// extensionsToTreatAsEsm: [".ts"],
-	// globals: {
-	// 	"ts-jest": {
-	// 		useESM: true,
-	// 	},
-	// },
-	// moduleNameMapper: {
-	// 	"^(\\.{1,2}/.*)\\.js$": "$1",
-	// },
 
 	testMatch: ["**/*.test.ts"],
 
 	collectCoverage: true,
 	collectCoverageFrom: ["src/**/*.ts"],
-
-	// // coverageProvider: ["v8"],
-	// coverageReporters: ["clover", "json", "lcov", "text"],
 
 	coverageThreshold: {
 		global: {
@@ -32,6 +19,5 @@ module.exports = {
 	},
 
 	coverageDirectory: "coverage",
-	// coverageProvider: "babel",
 	coverageProvider: "v8"
 };
