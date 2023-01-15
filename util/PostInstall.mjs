ba2a82151829d1c2473f0736db4ac017588553c8
CEbbinghaus <git@cebbinghaus.com>
2023-01-15 22:40:21 +1100

    Fixed Postinstall script when no typescript package is present (#23)

--- util/PostInstall.mjs
+++ util/PostInstall.mjs
@@ -62,17 +62,17 @@ if(!shouldCompile) {
 	process.exit(0);
 }
 
-import { Compile } from "./CompileProject.mjs";
 
 // Runs initial Build script if the lib folder doesn't exist (in case it was included as a git repository)
 console.log(
 	"Library directory is missing. Must be installing from Git. Building project from source"
-);
-
-await VerifyDependencies().catch((error) => {
-	throw error;
-});
-
+	);
+	
+	await VerifyDependencies().catch((error) => {
+		throw error;
+	});
+	
+const { Compile } = await import("./CompileProject.mjs");
 const Success = await Compile();
 
 if (!Success) console.error("Failed to Build Source");
