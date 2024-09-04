5747794f7cd0b12dc65052a2d4abe5a7a917e60b
CEbbinghaus <git@cebbinghaus.com>
2024-09-04 23:40:53 +1000

    Saving state of first efforts

--- docs/src/languages.ts
+++ docs/src/languages.ts
@@ -1,8 +1,12 @@
-import { KNOWN_LANGUAGES } from './config.js';
+import { KNOWN_LANGUAGES as CONFIG_KNOWN_LANGUAGES } from './config.js';
+
+export const KNOWN_LANGUAGES = CONFIG_KNOWN_LANGUAGES;
 
 export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES);
 
-export function getLanguageFromURL(pathname: string) {
-  const langCodeMatch = pathname.match(/\/([a-z]{2}-?[A-Z]{0,2})\//);
-  return langCodeMatch ? langCodeMatch[1] : 'en';
+export const langPathRegex = /\/([a-z]{2}-?[A-Z]{0,2})\//;
+
+export function getLanguageFromURL(pathname: string): string {
+	const langCodeMatch = pathname.match(/\/([a-z]{2}-?[A-Z]{0,2})\//);
+	return langCodeMatch ? langCodeMatch[1] : 'en';
 }
